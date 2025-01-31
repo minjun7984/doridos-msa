7ec215dadf5c43112dc2cfa8771b623818aa2999
jung min jun <alswns7984@naver.com>
2025-01-31 19:26:30 +0900

    feat: Spring Cloud, Eureka Server 활성화

---
+++ reservation-service/src/main/java/kr/doridos/reservationservice/ReservationServiceApplication.java
@@ -0,0 +1,19 @@
+package kr.doridos.reservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication (
+        scanBasePackages = { "kr.doridos.reservationservice", "kr.doridos.common"}
+)
+public class ReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationServiceApplication.class, args);
+    }
+
+}
